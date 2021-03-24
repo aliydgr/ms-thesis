c92c717fd4cdc79bbd804862f221923afbd88bbf
yadegari <yadegari@viratech.ir>
2021-03-24 08:22:40 +0430

    Add population method to factory

--- libs/factory.py
+++ libs/factory.py
@@ -46,15 +46,50 @@ _library_function_names_bio = [
 ]
 
 
+def _population(data, t):
+    a = 2
+    b = 3
+    beta = 1
+
+    n = len(data)
+    result = np.empty(n)
+
+    for i in range(0,n):
+        sigma = 0
+        for j in range (0,n):
+            sigma = sigma + (adjacency[i,j])*pow(data[j], a)
+        result[i] = sigma - beta*pow(data[i], b)
+
+    return result
+
+
+_library_functions_population = [
+    lambda : 1,
+    lambda x : x,
+    lambda x : x*x,
+    lambda x : x*x*x
+]
+_library_function_names_population = [
+    lambda : 1,
+    lambda x : x,
+    lambda x : x + '.' + x,
+    lambda x : x + '.' + x + '.' + x
+]
+
+
 def get_dynamic_function(type_):
     if type_ == 'bio':
         return _bio
+    if type_ == 'population':
+        return _population
     print('unsupported type')
 
 
 def get_custom_library(type_):
     if type_ == 'bio':
         return ps.CustomLibrary(library_functions=_library_functions_bio, function_names=_library_function_names_bio)
+    if type_ == 'population':
+        return ps.CustomLibrary(library_functions=_library_functions_population, function_names=_library_function_names_population)
     print('unsupported type')
 
 
